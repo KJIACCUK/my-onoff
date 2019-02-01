547a3c7b44e4236c53aa306187471fa6b97784f4
kjlaccuk uit <mig-alex14@mail.ru>
2019-02-01 11:02:00 +0300

    commit test new_branch_dev

--- basic/views/layouts/main.php
+++ basic/views/layouts/main.php
@@ -45,7 +45,7 @@ AppAsset::register($this);
                 <div class="menu-desktop">
                     <ul class="main-menu">
                         <li>
-                            <a href="<?=Url::home()?>">Главная</a>
+                            <a href="<?=Url::home()?>">Главная тест</a>
                         </li>
 
                         <li>
